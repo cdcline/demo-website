84743094231d0ecbdec49473ccd47f2744cb8985
Chris Cline <1248182+cdcline@users.noreply.github.com>
2022-04-30 07:50:00 -0700

    Break JS into classes
    JS gets super messy really fast, especially if we're gonna run
    "vanilla" which we should be able to do.
    
    IMO the best way to handle it is to break everything up into
    classes based on some theme and try to keep all those classes
    at a reasonable size (<1k lines).
    
    I'm still not ready for several files (and don't really want to
    support that) so this breaks all the JS logic into separate classes
    based on some theme but keeps all the classes in the same file.
    
    Part of #48

--- src/templates/js/page.js
+++ src/templates/js/page.js
@@ -1,27 +1,49 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
-function getRandomIntInclusive(min, max) {
-   min = Math.ceil(min);
-   max = Math.floor(max);
-   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
+class MathUtils {
+   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+   static getRandomIntInclusive(min, max) {
+      let min = Math.ceil(min);
+      let max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+   }
 
-function getRandomColor() {
-   return getRandomIntInclusive(0, 255);
-}
+   static getRandomColor() {
+      return this.getRandomIntInclusive(0, 255);
+   }
 
-function getRandomRGB() {
-   let r = getRandomColor();
-   let g = getRandomColor();
-   let b = getRandomColor();
-   return "rgb(" + r + "," + g + "," + b + ")";
-}
-
-function makeFunColors() {
-   const collection = document.getElementsByClassName("fun");
-   for (let i = 0; i < collection.length; i++) {
-      collection[i].style.color = getRandomRGB();
+   static getRandomRGB() {
+      let r = this.getRandomColor();
+      let g = this.getRandomColor();
+      let b = this.getRandomColor();
+      return "rgb(" + r + "," + g + "," + b + ")";
    }
 }
 
-makeFunColors();
-document.getElementById("fun-button").addEventListener("click", makeFunColors);
+class ServerUtils {
+   static addOnLoadFunction(func) {
+      window.addEventListener("load", func);
+   }
+
+   static addClickFunctionOnId(id, func) {
+      document.getElementById(id).addEventListener("click", func);
+   }
+}
+
+class FunUtils {
+   static makeFunColors() {
+      let collection = document.getElementsByClassName("fun");
+      for (let i = 0; i < collection.length; i++) {
+         collection[i].style.color = MathUtils.getRandomRGB();
+      }
+   }
+
+   static setupFun() {
+      // Change colors before we've loaded resources
+      this.makeFunColors()
+      // Change colors after we've loaded resources
+      ServerUtils.addOnLoadFunction(this.makeFunColors);
+      // Change colors when we click the 'fun-button'
+      ServerUtils.addClickFunctionOnId('fun-button', this.makeFunColors);
+   }
+}
+
+FunUtils.setupFun();
