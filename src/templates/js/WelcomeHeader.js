262d60e6d8000df44f9b1e3d9f9f82c71a3ca53f
Chris Cline <1248182+cdcline@users.noreply.github.com>
2022-07-13 18:35:53 -0700

    Design Beta: Animate Welcome Header Slash
    We have a cool clash image in the background that we'd like to
    move across the back and reset after it's passed.
    
    This adds the stuff needed to do that.
    
    Nothing to extra, just the image, basic HTML, CSS and JS to move it.
    
    Part of #108

--- src/templates/js/WelcomeHeader.js
+++ src/templates/js/WelcomeHeader.js
@@ -9,21 +9,23 @@ class WelcomeHeader {
          return fCircle;
       });
 
+      this.slashLoop = new SlashLoop(document.getElementById('welcome-header-slash-container'), document.getElementById('welcome-header-slash'));
+      this.slashLoop.loop();
    }
 }
 
 class FloatingCircle {
 	constructor(circleEl) {
       this.circleEl = circleEl;
-      this.intervalId = null;
+      this.timeoutId = null;
       this.resetValues();
    }
 
    loop() {
-      clearTimeout(this.intervalId);
+      clearTimeout(this.timeoutId);
       this.resetValues();
       this.move();
-      this.intervalId = setTimeout(this.loop.bind(this), this.loopTime * 1000);
+      this.timeoutId = setTimeout(this.loop.bind(this), this.loopTime * 1000);
    }
 
    move() {
@@ -36,5 +38,42 @@ class FloatingCircle {
       this.loopTime = MathUtils.getRandomIntInclusive(5, 15);
       this.toX = MathUtils.getRandomIntInclusive(-20, 20);
       this.toY = MathUtils.getRandomIntInclusive(-20, 20);
+   }
+}
+
+class SlashLoop {
+   constructor(slashContainer, slashEl) {
+      this.slashContainer = slashContainer;
+      this.slashContainer.addEventListener('transitionend', function(el) {
+         this.resetAnimation();
+      }.bind(this));
+      this.slashEl = slashEl;
+      this.timeoutId = null;
+   }
+
+   loop() {
+      this.setRandomTime();
+      if (this.timeoutId) {
+         clearTimeout(this.timeoutId);
+         this.resetAnimation();
+      }
+      this.move();
+      this.timeoutId = setTimeout(this.loop.bind(this), this.loopTime * 1000);
+   }
+
+   move() {
+      let sTransform = 'translateX(350%)';
+      this.slashEl.style.transform = sTransform;
+      this.slashEl.style.transitionDuration = this.loopTime + 's';
+   }
+
+   setRandomTime() {
+      this.loopTime = MathUtils.getRandomIntInclusive(5, 15);
+   }
+
+   resetAnimation() {
+      let sTransform = 'translateX(0)';
+      this.slashEl.style.transform = sTransform;
+      this.slashEl.style.transitionDuration = '0s';
    }
 }
