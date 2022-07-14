c399560376ecfd4d1635beb0a3a8ca5bbeaca1dc
Chris Cline <1248182+cdcline@users.noreply.github.com>
2022-07-14 16:57:31 -0700

    Design Beta: Animate Welcome Header X Bar
    We want the Welcome Header to have cool animations. The Header has
    a set of 4 X images in a line that we'd like float kinda slowly in
    a random direction before going back to the set 4 position again.
    
    This adds the X png and the html, css and js to do that.
    
    It also alters the Header CSS to be a standard height which I think
    is the finalish height we want to use. It also fixes up the slash
    image to match the new height.
    
    Part of #108

--- src/templates/js/WelcomeHeader.js
+++ src/templates/js/WelcomeHeader.js
@@ -3,14 +3,24 @@ class WelcomeHeader {
    static loopCircles;
 
    static init() {
-      this.loopCircles = [...document.getElementsByClassName('js-floating-circle')].forEach(function(el) {
-         let fCircle = new FloatingCircle(el);
+      this.loopCircles = [...document.getElementsByClassName('js-floating-circle')].map(function(el) {
+         var fCircle = new FloatingCircle(el);
          fCircle.loop();
          return fCircle;
       });
 
-      this.slashLoop = new SlashLoop(document.getElementById('welcome-header-slash-container'), document.getElementById('welcome-header-slash'));
-      this.slashLoop.loop();
+      this.slashLoops = [...document.getElementsByClassName('js-welcome-header-slash-container')].map(function(el) {
+         let sLoop = new SlashLoop(el);
+         sLoop.loop();
+         return sLoop;
+      }.bind(this));
+
+      this.floatXBoxes = [...document.getElementsByClassName('js-welcome-header-x-container')].map(function(el) {
+         let xBox = new FloatingXBoxContainer(el);
+         xBox.loop();
+         return xBox;
+
+      });
    }
 }
 
@@ -42,12 +52,12 @@ class FloatingCircle {
 }
 
 class SlashLoop {
-   constructor(slashContainer, slashEl) {
+   constructor(slashContainer) {
       this.slashContainer = slashContainer;
       this.slashContainer.addEventListener('transitionend', function(el) {
          this.resetAnimation();
       }.bind(this));
-      this.slashEl = slashEl;
+      this.slashEl = this.slashContainer.querySelector('.js-moving-slash');
       this.timeoutId = null;
    }
 
@@ -62,7 +72,7 @@ class SlashLoop {
    }
 
    move() {
-      let sTransform = 'translateX(350%)';
+      let sTransform = 'translateX(285%)';
       this.slashEl.style.transform = sTransform;
       this.slashEl.style.transitionDuration = this.loopTime + 's';
    }
@@ -75,5 +85,70 @@ class SlashLoop {
       let sTransform = 'translateX(0)';
       this.slashEl.style.transform = sTransform;
       this.slashEl.style.transitionDuration = '0s';
+   }
+}
+
+class FloatingXBoxContainer {
+   constructor(xBoxContainer) {
+      this.xBoxContainer = xBoxContainer;
+      this.xBoxes = [...this.xBoxContainer.querySelectorAll('.js-floating-x')].map(function(el) {
+         let fXBox = new FloatingXBox(el);
+         return fXBox;
+      });
+      this.timeoutId = null;
+   }
+
+   loop() {
+      this.setRandomTime();
+      if (this.timeoutId) {
+         clearTimeout(this.timeoutId);
+      }
+      this.move();
+      this.timeoutId = setTimeout(this.loop.bind(this), this.loopTime * 1000);
+   }
+
+   move() {
+      this.xBoxes.forEach(function(fXBox) {
+         fXBox.setupFloat(this.loopTime);
+      }.bind(this));
+   }
+
+   setRandomTime() {
+      this.loopTime = MathUtils.getRandomIntInclusive(5, 15);
+   }
+}
+
+class FloatingXBox {
+   constructor(xBoxEl) {
+      this.xBox = xBoxEl;
+      this.resetTimeoutid = null;
+   }
+
+   setupFloat(maxFloatTime) {
+      // Randomly don't have one move
+      let shouldMove = MathUtils.getRandomIntInclusive(0, 2);
+      if (!shouldMove) {
+         return;
+      }
+      this.maxMoveTime = parseInt(maxFloatTime / 2);
+      this.floatToRandomSpot();
+      this.resetTimeoutid = setTimeout(this.floatHome.bind(this), this.maxMoveTime * 1000);
+   }
+
+   floatToRandomSpot() {
+      let toX = MathUtils.getRandomIntInclusive(-50, 50);
+      let toY = MathUtils.getRandomIntInclusive(-50, 50);
+      let sTransform ='translate(' + toX + '%,' + toY+ '%)';
+      this.xBox.style.transform = sTransform;
+      // Kinda arbitrary but this will change x move speeds
+      // If > "maxMoveTime" the X won't actually go the full set distance
+      // This duration gives a pretty good, random, slow "floaty" feel
+      let duration = this.maxMoveTime * 2 + 's';
+      this.xBox.style.transitionDuration = duration;
+   }
+
+   floatHome() {
+      let sTransform ='translate(0%, 0%)';
+      this.xBox.style.transform = sTransform;
    }
 }
