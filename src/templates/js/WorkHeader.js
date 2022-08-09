095a2ccf8fcfed3c4e74d0863a181a67f90cc812
Chris Cline <1248182+cdcline@users.noreply.github.com>
2022-08-09 13:33:58 -0700

    Design Beta: Add color change to Work Header Shape
    The work page header has a shape in the background that slowly
    changes colors over a gradient.
    
    I thought it would be cool if the colors would start to randomly
    change over time, so I added that.
    
    Now the first "loop" will keep the initial colors but the end
    of the second "loop" will end up on different colors.
    
    Each loop going forward will shift into new random colors through
    the gradiant.
    
    It's a bit of a silly hack to blow up the background image to a
    huge size and switch out the gradiants but it seems to work pretty
    well and doesn't require too much logic.
    
    Part of #110

--- src/templates/js/WorkHeader.js
+++ src/templates/js/WorkHeader.js
@@ -1,15 +1,54 @@
 class WorkHeader {
-   static shape;
+   static backgroundShape;
 
    static init() {
-      this.shape = document.getElementById('header-curve');
-      this.shape.classList.add('js-animate-curve');
-      document.getElementById('work-header-background-container').addEventListener('transitionend', function(ev) {
+      this.backgroundShape = new BackgroundShape(document.getElementById('work-header-background-container'));
+   }
+}
+
+class BackgroundShape {
+   constructor(shapeContainer) {
+      this.shapeContainer = shapeContainer;
+      this.shape = this.shapeContainer.querySelector('.curve');
+      // It's ugly to hardcode these but calculating it is a pain.
+      this.colors = ['#8e2de2', '#4a00e0', '#8e2de2', '#4a00e0'];
+
+      this.shapeContainer.addEventListener('transitionend', function(ev) {
          let isAnimation = ev.propertyName === 'background-position-x';
          if (isAnimation) {
-            this.shape.classList.toggle('js-animate-curve');
+            if (this.animatingRight) {
+               this.animateLeft();
+            } else {
+               this.animateRight(/*resetColors*/true);
+            }
+         } else {
+
          }
       }.bind(this));
 
+      this.animateRight(/*resetColors*/false);
+   }
+
+   animateRight(resetColors) {
+      if (resetColors) {
+         this.colors[2] = MathUtils.getRandomRGB();
+         this.colors[3] = MathUtils.getRandomRGB();
+      }
+      this.shape.style.background = this.buildGrandiant();
+      this.shape.classList.add('js-animate-curve');
+      this.animatingRight = true;
+   }
+
+   animateLeft() {
+      this.colors[0] = MathUtils.getRandomRGB();
+      this.colors[1] = MathUtils.getRandomRGB();
+      this.shape.style.background = this.buildGrandiant();
+      this.shape.classList.remove('js-animate-curve');
+      this.animatingRight = false;
+   }
+
+   buildGrandiant() {
+      let colorStr = this.colors.join(', ');
+      this.shape.style.backgroundImage = 'linear-gradient(to left, ' + colorStr + ')';
    }
 }
